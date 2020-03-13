81b352ad2ffe39faf40dfc6731e86217908db1b3
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2020-03-13 09:06:55 +0100

    inserted frontend

---
+++ src/main/webapp/precache-manifest.02767cc28bb0eb070a2c367f49312e53.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee650d4b5abe368072b1abd173f827b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a754924c0139173e7c77",
+    "url": "/static/css/main.6a032797.chunk.css"
+  },
+  {
+    "revision": "0d245902b0f69200bf2f",
+    "url": "/static/js/2.2cfc624d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2cfc624d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a754924c0139173e7c77",
+    "url": "/static/js/main.00a83f06.chunk.js"
+  },
+  {
+    "revision": "076327392cca8ea8f80c",
+    "url": "/static/js/runtime-main.0a266b08.js"
+  },
+  {
+    "revision": "c20034cb743cc2822b9b2b8bd878fe72",
+    "url": "/static/media/cold.c20034cb.jpg"
+  },
+  {
+    "revision": "5fc295d0cff5c112b162c349be360e0f",
+    "url": "/static/media/cooking.5fc295d0.jpg"
+  },
+  {
+    "revision": "67bd6ffe8388b04b26026b6cbaba655f",
+    "url": "/static/media/dessert.67bd6ffe.jpg"
+  },
+  {
+    "revision": "0f39eccbd067384fe9246810d2e2249f",
+    "url": "/static/media/hamburger.0f39eccb.jpg"
+  },
+  {
+    "revision": "767d44b1ee0a88fbc6369f36acd7410e",
+    "url": "/static/media/hot.767d44b1.jpg"
+  },
+  {
+    "revision": "91ead9f09cf20e26f438770032ea03da",
+    "url": "/static/media/hotdog.91ead9f0.jpg"
+  },
+  {
+    "revision": "c62d55a42ca98c156b5d98150072317d",
+    "url": "/static/media/kebab.c62d55a4.jpg"
+  },
+  {
+    "revision": "dea7d8f4a4a6d5555376b5ff03c0d1fa",
+    "url": "/static/media/mugcake.dea7d8f4.jpg"
+  },
+  {
+    "revision": "d59ea5f6ec8b77b4841c377e8137cdb9",
+    "url": "/static/media/pancake.d59ea5f6.jpg"
+  },
+  {
+    "revision": "7dadefb4fc48c697f60a80f53c1bf5ac",
+    "url": "/static/media/snacks.7dadefb4.jpg"
+  },
+  {
+    "revision": "b7241620e4b795f0e47a3b1c93b145be",
+    "url": "/static/media/wings.b7241620.jpg"
+  }
+]);
